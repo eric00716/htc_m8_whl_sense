c0da378cb9bd09f80e738f4d64f6b125f144826f
flar2 <asegaert@gmail.com>
2014-04-06 10:06:04 -0400

    Downgrade CIFS
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Conflicts:
    	fs/cifs/asn1.c
    	fs/cifs/cifs_debug.h
    	fs/cifs/cifs_dfs_ref.c
    	fs/cifs/cifs_fs_sb.h
    	fs/cifs/cifs_unicode.c
    	fs/cifs/cifsacl.c
    	fs/cifs/cifsencrypt.c
    	fs/cifs/cifsfs.c
    	fs/cifs/cifsfs.h
    	fs/cifs/cifsglob.h
    	fs/cifs/cifspdu.h
    	fs/cifs/cifsproto.h
    	fs/cifs/cifssmb.c
    	fs/cifs/connect.c
    	fs/cifs/dir.c
    	fs/cifs/export.c
    	fs/cifs/file.c
    	fs/cifs/inode.c
    	fs/cifs/misc.c
    	fs/cifs/readdir.c
    	fs/cifs/sess.c
    	fs/cifs/smbencrypt.c
    	fs/cifs/transport.c
    	fs/cifs/xattr.c

--- fs/cifs/cifs_dfs_ref.c
+++ fs/cifs/cifs_dfs_ref.c
@@ -141,8 +141,8 @@ char *cifs_compose_mount_options(const char *sb_mountdata,
 
 	rc = dns_resolve_server_name_to_ip(*devname, &srvIP);
 	if (rc < 0) {
-		cFYI(1, "%s: Failed to resolve server part of %s to IP: %d",
-			__func__, *devname, rc);
+		cERROR(1, "%s: Failed to resolve server part of %s to IP: %d",
+			  __func__, *devname, rc);
 		goto compose_mount_options_err;
 	}
 
@@ -226,6 +226,8 @@ compose_mount_options_out:
 compose_mount_options_err:
 	kfree(mountdata);
 	mountdata = ERR_PTR(rc);
+	kfree(*devname);
+	*devname = NULL;
 	goto compose_mount_options_out;
 }
 
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.0-g0228c54-dirty"
+#define UTS_RELEASE "3.4.0-g6d8cbed-dirty"
