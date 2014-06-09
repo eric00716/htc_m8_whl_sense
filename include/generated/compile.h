a53b029d13fae2330814b79b429e8b3b505da6f9
MassStash <MassStash@gmail.com>
2014-06-09 18:10:57 -0500

    Add built crap and touch on cook script

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 18 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#18 SMP PREEMPT Mon Jun 9 17:48:21 CDT 2014"
+#define LINUX_COMPILE_BY "zaichron-lin"
+#define LINUX_COMPILE_HOST "zaichron-lin-pc"
+#define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (crosstool-NG linaro-1.13.1+bzr2663 - Linaro GCC 2014.05 - Cortex-A15) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-g4b0457e"
