0228c54ad391a8980a68c2a51b5740e663a1ce81
MassStash <MassStash@gmail.com>
2014-06-20 13:04:40 -0500

    RM crap

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 23 */
+/* This file is auto generated, version 28 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#23 SMP PREEMPT Tue Jun 17 02:09:13 CDT 2014"
+#define UTS_VERSION "#28 SMP PREEMPT Fri Jun 20 06:07:45 CDT 2014"
 #define LINUX_COMPILE_BY "zaichron-lin"
 #define LINUX_COMPILE_HOST "zaichron-lin-pc"
-#define LINUX_COMPILER "gcc version 4.9.1 20140505 (prerelease) (crosstool-NG linaro-1.13.1+bzr2663 - Linaro GCC 2014.05 - Cortex-A15) "
+#define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
