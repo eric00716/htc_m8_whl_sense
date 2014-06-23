3671eedb64d1a73a5128efe5cd78e3e705c5d62e
MassStash <MassStash@gmail.com>
2014-06-23 13:41:40 -0500

    Update baconMaker with version numbers

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 30 */
+/* This file is auto generated, version 31 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#30 SMP PREEMPT Sat Jun 21 12:08:56 CDT 2014"
-#define LINUX_COMPILE_BY "zaichron-lin"
+#define UTS_VERSION "#31 SMP PREEMPT Mon Jun 23 13:19:54 CDT 2014"
+#define LINUX_COMPILE_BY "MassStash"
 #define LINUX_COMPILE_HOST "zaichron-lin-pc"
 #define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
