2616bd13865993f4c127c2cce142e96d2b1f189a
MassStash <MassStash@gmail.com>
2014-06-23 12:31:45 -0500

    Update defconfig: Version number

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 28 */
+/* This file is auto generated, version 30 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#28 SMP PREEMPT Fri Jun 20 06:07:45 CDT 2014"
+#define UTS_VERSION "#30 SMP PREEMPT Sat Jun 21 12:08:56 CDT 2014"
 #define LINUX_COMPILE_BY "zaichron-lin"
 #define LINUX_COMPILE_HOST "zaichron-lin-pc"
 #define LINUX_COMPILER "gcc version 4.9.1 20140529 (prerelease) (crosstool-NG linaro-1.13.1+bzr2665 - Linaro GCC 2014.06 - Cortex-A15) "
